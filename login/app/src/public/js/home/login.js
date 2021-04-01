3f5fe82ff91fbd3a8b080a94eee811a7457cb8b8
ParkSeungSu <cldkr0401@naver.com>
2021-04-01 15:16:28 +0900

    0401-1516
    DOM - Document Object Model
                refer

--- login/app/src/public/js/home/login.js
+++ login/app/src/public/js/home/login.js
@@ -1,3 +1,22 @@
 "use strict";
 
+//login function
+
+// 로그인 버튼 클릭시 ID PW 값을 서버로 전송
+//DOM -> Document Object Model 
+
+const id = document.querySelector("#id"),
+psword = document.querySelector("#password"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req={
+        id:id.value,
+        password:psword.value,
+    };
+    console.log(req);
+}
+console.log(id);
 console.log("hello");
