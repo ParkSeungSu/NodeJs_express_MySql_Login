8f6e6622e1515a724396f5e7ee5d88361a7941d8
ParkSeungSu <cldkr0401@naver.com>
2021-04-05 11:11:19 +0900

    0405-1111
    winston log management

--- login/app/app.js
+++ login/app/app.js
@@ -4,19 +4,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 
 
 // 협업시에는 운영체제마다 환경 변수를 적용하는 방법이 다름
 const app = express();
 dotenv.config();
 
-const accessLogStream = require('./src/config/log');
+// const accessLogStream = require('./src/config/log');
 
 //routing
 const home = require("./src/routes/home");
-
-
 
 //app setting
 app.set("views", "./src/views");
@@ -26,8 +24,8 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 //URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use(morgan('dev',{ stream: accessLogStream }));
+// app.use(morgan('dev'));
+// app.use(morgan('dev',{ stream: accessLogStream }));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
 
